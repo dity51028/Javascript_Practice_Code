45387850ca18863c01d395da1565afb26f3a5c15
Dipannita_Mondal <dipannitamisti10@gmail.com>
2023-04-21 13:12:05 +0530

    Working with arrays,objects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 let defaultResult = 0
 let currentResult = defaultResult
+let logEntries = []
 
 function getUserNumberInput()
 {
@@ -11,33 +12,49 @@ function createOutput(operator,resultBeforeCalc,calcNumber){
     outputResult(currentResult,description)
 
 }
+function writeToLog(operationIdetifier,prevresult,operationNumber,newresult)
+{
+    const logEntry = {
+        operation : operationIdetifier,
+        prevResult : prevresult,
+        number : operationNumber,
+        result : newresult
+    }
+    logEntries.push(logEntry)
+    console.log(logEntries)
+}
 
 function add(){
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
-    currentResult =(currentResult) + enteredNumber
+    currentResult +=  enteredNumber
     createOutput('+',initialResult,enteredNumber)
+    writeToLog('ADD',initialResult,enteredNumber,currentResult)
 }  
 
 function subtract(){
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
-    currentResult =(currentResult) - enteredNumber
+    currentResult -= enteredNumber
     createOutput('-',initialResult,enteredNumber)
+    writeToLog('Subtract',initialResult,enteredNumber,currentResult)
+    
 
 }
 function multiply(){
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
-    currentResult =(currentResult) * enteredNumber
+    currentResult *= enteredNumber
     createOutput('*',initialResult,enteredNumber)
+    writeToLog('Multiply',initialResult,enteredNumber,currentResult)
 
 }
 function divide(){
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
-    currentResult =(currentResult) / enteredNumber
+    currentResult  /= enteredNumber
     createOutput('/',initialResult,enteredNumber)
+    writeToLog('Division',initialResult,enteredNumber,currentResult)
 
 }
 addBtn.addEventListener('click',add)
